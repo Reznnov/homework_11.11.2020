b295b1662feaa0c5ceba415ec9c520d528079087
Reznnov <64481216+Reznnov@users.noreply.github.com>
2020-11-12 11:49:25 +0300

    Add files via upload
    l

--- ft_sum_even_lst.py
+++ ft_sum_even_lst.py
@@ -1,5 +1,12 @@
+def ft_len(str):
+    num = 0
+    for i in str:
+        num += 1
+    return num
+
+
 def ft_sum_even_lst(lst):
-    dlin = len(lst)
+    dlin = ft_len(lst)
     summ = 0
     for i in range(dlin - 1):
         if i % 2 == 0:
