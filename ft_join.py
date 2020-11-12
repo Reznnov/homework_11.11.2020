6d1219c9818226f4e6cde9506047b56fcecdafd4
Reznnov <64481216+Reznnov@users.noreply.github.com>
2020-11-12 12:28:27 +0300

    ljkl

--- ft_join.py
+++ ft_join.py
@@ -1,12 +1,12 @@
-def ft_len(str):
+def ft_len(s):
     num = 0
-    for i in str:
+    for i in s:
         num += 1
     return num
 
 
 def ft_join(lst, sep=" "):
-    strr = ""
+    strr = ''
     dlin = ft_len(lst)
     for i in (dlin):
         if i == dlin - 1:
